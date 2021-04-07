a75de75dd0460d9c6f1331941e65bbf8309ad8c4
dzemildupljak <dzemildupljak4795@gmail.com>
2021-04-07 21:22:04 +0200

    minor chages with heroku configs

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uvicorn blog.main:app --port 80
+web: uvicorn blog.main:app --host=0.0.0.0 --port=${PORT: -8000}
--- blog/database.py
+++ blog/database.py
@@ -2,17 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost:5432/centarnit_db"
-# SQLALCHEMY_DATABASE_URL = "postgres://jxtqnmnpzqkfqf:7303b16637618897b0264429c0b79a2aca8de6b3d484a56f2bb4f8b25c6fa7c7@ec2-34-254-69-72.eu-west-1.compute.amazonaws.com:5432/d8u7aj6ruinb2g"
-SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost:5432/centarnit_db"
+# # SQLALCHEMY_DATABASE_URL = "postgres://jxtqnmnpzqkfqf:7303b16637618897b0264429c0b79a2aca8de6b3d484a56f2bb4f8b25c6fa7c7@ec2-34-254-69-72.eu-west-1.compute.amazonaws.com:5432/d8u7aj6ruinb2g"
+# SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
 
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL
+# )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
