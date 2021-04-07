9e6b060a2b9d9172b7387b9eeb4cb2263d4ed57f
dzemildupljak <dzemildupljak4795@gmail.com>
2021-04-07 21:53:27 +0200

    change database connection

--- blog/database.py
+++ blog/database.py
@@ -2,16 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost:5432/centarnit_db"
-# SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
 # engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 # )
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
