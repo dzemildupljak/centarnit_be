f060ff403fc050403252c67061cce5b4ad0fc72c
dzemildupljak <dzemildupljak4795@gmail.com>
2021-04-08 19:02:39 +0200

    refactoring imports in everything

--- blog/helpers/JWToken.py
+++ blog/helpers/JWToken.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from typing import Optional
 from jose import jwt
 from jose.exceptions import JWTError
-from blog.schemas.token import TokenData
+from blog.schemas import token
 
 
 SECRET_KEY = "29d54264f4b9340762e7d81d523e1a1931802b3bc706a65480e3817b84b0433a"
@@ -21,12 +21,12 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     return encoded_jwt
 
 
-def verify_token(token: str, credentials_exception):
+def verify_token(tkn: str, credentials_exception):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(tkn, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-        token_data = TokenData(username=username)
+        token_data = token.TokenData(username=username)
     except JWTError:
         raise credentials_exception
