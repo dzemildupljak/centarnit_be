7434a8e3180159fdbf943d996218fcf80b242126
dzemildupljak <dzemildupljak4795@gmail.com>
2021-04-08 14:06:36 +0200

    finished conifg for heroku

--- blog/database.py
+++ blog/database.py
@@ -2,15 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost:5432/centarnit_db"
-print('DATABASE_URL .get()', os.environ.get('DATABASE_URL'))
-print('DATABASE_URL []', 'postgresql' + os.environ['DATABASE_URL'][8:])
+
 SQLALCHEMY_DATABASE_URL = 'postgresql' + os.environ['DATABASE_URL'][8:]
 
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
