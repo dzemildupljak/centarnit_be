7348936dcdadcc44029a12256f3b9255e784aae3
dzemildupljak <dzemildupljak4795@gmail.com>
2021-05-03 13:27:00 +0200

    changes with docker flow and some alembic init values

--- blog/helpers/secretkey_generator.py
+++ blog/helpers/secretkey_generator.py
@@ -1,6 +1,13 @@
+from passlib.context import CryptContext
 from secrets import token_bytes, token_hex
 from base64 import b64encode
 
-print(b64encode(token_bytes(32)).decode())
-print(30*'-')
-print(token_hex(32))
+# print(b64encode(token_bytes(32)).decode())
+# print(30*'-')
+# print(token_hex(32))
+
+
+pwd_cxt = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+sifra = pwd_cxt.hash('sysadmin')
+print(sifra)
